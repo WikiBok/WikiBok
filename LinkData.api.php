ddc74fa96e57e1d5e0cb6d89bcf836297e6c35c2
junzo.nakajima <sigihara_subaru@hotmail.com>
2012-10-26 18:41:51 +0900

    api-update 	modified:   LinkData.api.php

--- LinkData.api.php
+++ LinkData.api.php
@@ -19,25 +19,28 @@ class LinkDataApi extends ApiBase {
 		//戻り値の設定
 		$result = $this->getResult();
 		$data = array();
+		$bokdata = array();
 
 		//BOK-XML
-		if($this->params['ldbokdata']) {
-			$data = $this->getBokData();
-			if(count($data)) {
-				$result->addValue(null,$this->getModuleName(),array('bok'=>array('count'=>count($data))));
-				$result->setIndexedTagName($data,'link');
-				$result->addValue($this->getModuleName(),'bok',$data);
-			}
+		if(!$this->params['ldonlysmw']) {
+			$bokdata = $this->getBokData();
+			$bokdata = array_filter($bokdata,array(__CLASS__,'filterBok'));
 		}
-		//データ取得
-		$data = $this->getSmwData();
-		if(count($data)) {
-			$result->addValue(null,$this->getModuleName(),array('smw'=>array('count'=>count($data))));
+		//SMW-LINKS
+		$smwdata = $this->getSmwData();
+		//MERGE
+		$data = array_merge($bokdata,$smwdata);
+		if(count($data) > 0) {
+			//各リンクの件数
+			$result->addValue(null,$this->getModuleName(),array('links'=>array('bok'=>count($bokdata),'smw'=>count($smwdata))));
 			$result->setIndexedTagName($data,'link');
-			$result->addValue($this->getModuleName(),'smw',$data);
+			$result->addValue($this->getModuleName(),'links',$data);
 		}
 		
 		return true;
+	}
+	private function filterBok($v) {
+		return (isset($v['filter']) && ($v['filter']));
 	}
 	/**
 	 * BOK-XMLデータをリンク形式で取得
@@ -48,9 +51,33 @@ class LinkDataApi extends ApiBase {
 		$data = $db->getBokHead();
 		if($data !== false) {
 			$xml = new BokXml($data['bok']);
-			$res = $xml->getListData();
+			$_res = $xml->getListData();
+			foreach($_res as $rec) {
+				$a_flg = array();
+				if($this->params['ldsource']) {
+					$check = $this->params['ldsource'];
+					$a_flg[] = ((array_search($rec['source'],$check) > -1)
+								||(array_search($rec['target'],$check) > -1));
+				}
+				if($this->params['ldtarget']) {
+					$check = $this->params['ldtarget'];
+					$a_flg[] = ((array_search($rec['source'],$check) > -1)
+								||(array_search($rec['target'],$check) > -1));
+				}
+				if($this->params['ldnode'] && !isset($rec['filter'])) {
+					$check = $this->params['ldnode'];
+					$a_flg[] = ((array_search($rec['source'],$check) > -1)
+								||(array_search($rec['target'],$check) > -1));
+				}
+				$rec['filter'] = (count(array_filter($a_flg)) > 0);
+				$res[] = $rec;
+			}
 		}
 		return $res;
+	}
+	private function bokmask($v,$k,$add) {
+		list($_key,$_val) = $add;
+		return ($v[$_key] == $_val);
 	}
 	/**
 	 * パラメータを元にWHERE句を作成...
@@ -96,16 +123,26 @@ class LinkDataApi extends ApiBase {
 		//パラメータを元にデータを限定
 		$where = array();
 		$where = array_merge($where, $this->setWhere('p_id.smw_title',$this->params['ldlinkname']));
-		$where = array_merge($where,$this->setWhere('s_id.smw_title',$this->params['ldsource']));
-		$where = array_merge($where,$this->setWhere('o_id.smw_title',$this->params['ldtarget']));
 
 		//名前空間はデフォルト値あり
 		$ns = (empty($this->params['ldnamespace'])) ? array(NS_SPECIAL_DESCRIPTION) : $this->params['ldnamespace'];
 		$where = array_merge($where,$this->setWhere('s_id.smw_namespace',$ns,false));
 		$where = array_merge($where,$this->setWhere('o_id.smw_namespace',$ns,false));
-		if(count($where)) {
-			$query .= 'WHERE '.implode(' AND ',$where);
+		if(empty($this->params['ldnode'])) {
+			$where = array_merge($where, $this->setWhere('s_id.smw_title',$this->params['ldsource']));
+			$where = array_merge($where, $this->setWhere('o_id.smw_title',$this->params['ldtarget']));
 		}
+		else {
+			$arrayOr = array();
+			$arrayOr = array_merge($arrayOr, $this->setWhere('s_id.smw_title',$this->params['ldnode']));
+			$arrayOr = array_merge($arrayOr, $this->setWhere('o_id.smw_title',$this->params['ldnode']));
+			$where = array_merge($where,array('('.implode(' OR ',$arrayOr).')'));
+		}
+		if(count($where)) {
+			$query .= ' WHERE '.implode(' AND ',$where);
+		}
+
+
 		if($this->params['ldlimit']) {
 			$query .= ' LIMIT '.$this->params['ldlimit'];
 		}
@@ -132,12 +169,7 @@ class LinkDataApi extends ApiBase {
 	private function getRevDB() {
 		if (!isset ($this->mRevisionDB)) {
 			$this->profileDBIn();
-			$this->mRevisionDB = new RevisionDB(
-				BOK_DATABASE_HOST,
-				BOK_DATABASE_DB,
-				BOK_DATABASE_USER,
-				BOK_DATABASE_PASS
-			);
+			$this->mRevisionDB = new RevisionDB();
 			$this->profileDBOut();
 		}
 		return $this->mRevisionDB;
@@ -164,7 +196,8 @@ class LinkDataApi extends ApiBase {
 	 */
 	public function getDescription() {
 		return array(
-			'This module is used to get description link data.'
+			'This module is used to get description link data.',
+			'This module treat a bok-link as two links.(parent->child ans child->parent)'
 		);
 	}
 	/**
@@ -172,12 +205,13 @@ class LinkDataApi extends ApiBase {
 	 */
 	public function getAllowedParams() {
 		return array(
-			'ldlinkname' => array(ApiBase::PARAM_TYPE => 'string' ,ApiBase::PARAM_ISMULTI => true,ApiBase::PARAM_ALLOW_DUPLICATES => true),
+			'ldnode'     => array(ApiBase::PARAM_TYPE => 'string' ,ApiBase::PARAM_ISMULTI => true,ApiBase::PARAM_ALLOW_DUPLICATES => true),
 			'ldsource'   => array(ApiBase::PARAM_TYPE => 'string' ,ApiBase::PARAM_ISMULTI => true,ApiBase::PARAM_ALLOW_DUPLICATES => true),
 			'ldtarget'   => array(ApiBase::PARAM_TYPE => 'string' ,ApiBase::PARAM_ISMULTI => true,ApiBase::PARAM_ALLOW_DUPLICATES => true),
 			'ldnamespace'=> array(ApiBase::PARAM_TYPE => 'integer',ApiBase::PARAM_ISMULTI => true,ApiBase::PARAM_ALLOW_DUPLICATES => true),
-			'ldlimit'=>array(ApiBase::PARAM_TYPE => 'integer',ApiBase::PARAM_MIN => 1),
-			'ldbokdata'=>false
+			'ldlimit'    => array(ApiBase::PARAM_TYPE => 'integer',ApiBase::PARAM_MIN => 1),
+			'ldlinkname' => array(ApiBase::PARAM_TYPE => 'string' ,ApiBase::PARAM_ISMULTI => true,ApiBase::PARAM_ALLOW_DUPLICATES => true),
+			'ldonlysmw'  => false,
 		);
 	}
 	/**
@@ -186,11 +220,12 @@ class LinkDataApi extends ApiBase {
 	public function getParamDescription() {
 		return array(
 			'ldlinkname' => 'A list of linkname.',
-			'ldsource' => 'A list of titles to linked from.',
-			'ldtarget' => 'A list of titles to linked to.',
+			'ldsource' => 'A list of titles to smw-link-from.',
+			'ldtarget' => 'A list of titles to smw-link-to.',
+			'ldnode' => 'A list of titles to smw-link-form/smw-link-to.',
 			'ldnamespace'=>array('Show links in this namespace(s) only.','Default:'.NS_SPECIAL_DESCRIPTION),
 			'ldlimit'=>array('How many links to return.'),
-			'ldbokdata'=>"Get link from WikiBOK-XML's Data."
+			'ldonlysmw'=>'result is only smw-links.[no bok-links]'
 		);
 	}
 	/**
@@ -198,8 +233,14 @@ class LinkDataApi extends ApiBase {
 	 */
 	public function getExamples() {
 		return array(
-			'Get links pages',
-			'  api.php?action=wikibok'
+			'Get all links',
+			'  api.php?action=wikibok',
+			"Get target page's links", //'
+			'  api.php?action=wikibok&ldnode=Main%20Page',
+			"Get target page's smw-links", //'
+			'  api.php?action=wikibok&ldonlysmw&ldnode=Main%20Page',
+			"Get target page's link data", //'
+			'  api.php?action=wikibok&ldsource=Main%20Page',
 		);
 	}
 
